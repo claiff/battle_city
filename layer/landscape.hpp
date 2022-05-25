dff5b7eb62030cdaeddea54663b8a0b520ce606d
claiff <claiff@mail.ru>
2022-05-25 17:41:40 +0300

    Рефакторил слой landscape

--- layer/landscape.hpp
+++ layer/landscape.hpp
@@ -26,9 +26,17 @@ namespace layer
 
 		void draw( sf::RenderTarget& target, sf::RenderStates const& states ) const override;
 	private:
+		[[nodiscard]] sf::Sprite GetSprite( resource::Id::Landscape landscape, sf::Vector2f const& shift ) const;
+		void ApplyShift( sf::Vector2f& shift, sf::Sprite const& sprite ) const;
+		sf::Vector2f GetSpriteSize( sf::Sprite const& sprite ) const;
+		[[nodiscard]] bool IsOutXBorder( const sf::Vector2f& shift, const sf::Vector2f& sprite_size ) const noexcept;
+		void ApplyNextLine( sf::Vector2f& shift, const sf::Vector2f& sprite_size ) const;
+
 		unsigned int mLevel;
 		sf::FloatRect mGameField;
 		std::list < resource::Id::Landscape > mLandscapes;
 		resource::Manager < resource::Id::Landscape > mSpriteManager;
+
+
 	};
 }
